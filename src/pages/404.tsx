59bb16f8e51922d3271c28e16437d805b5ef83be
Marc-Antoine Agboton <marc-antoine@MarcAntoinesMBP.attlocal.net>
2025-10-24 19:09:01 -0500

    fix: finalized 404 export

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        fontSize: "1.5rem",
+        color: "white",
+        background: "black",
+      }}
+    >
+      <h1>404 | Page Not Found</h1>
+    </div>
+  );
+}
+
